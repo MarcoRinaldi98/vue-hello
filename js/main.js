7794ce0c8aefcbef60b0c291073dfe5c69b28d17
Marco Rinaldi <marcorinaldi98@libero.it>
2023-03-14 13:29:04 +0100

    aggiuntamethods per cambio colore + icon

--- js/main.js
+++ js/main.js
@@ -10,8 +10,18 @@ createApp(
             return {
                 message: 'BUONGIORNO !',
                 image: 'img/lion.jpg',
-                color: 'text-primary'
+                color: 'text-primary',
+                icon: 'fa-solid fa-sun'
             }          
+        },
+        methods: {
+            changeColor() {
+                if (this.color = 'text-primary') {
+                    this.color = 'text-warning';
+                } else {
+                    this.color = 'text-primary';
+                }
+            }
         }
     }
 ).mount('#app')
